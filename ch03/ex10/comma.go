52d096d47ace52bd17fc5d1bcf4660b52b70735b
Ino Ayaka <60222003+InoAyaka@users.noreply.github.com>
2020-08-22 19:30:26 +0900

    ch03 ex10の修正

--- ch03/ex10/comma.go
+++ ch03/ex10/comma.go
@@ -1,19 +1,36 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 )
 
 func comma(s string) string {
-	n := len(s)
 
-	if n <= 3 {
+	if len(s) <= 3 {
 		return s
 	}
 
-	return comma(s[:n-3]) + "," + s[n-3:]
+	var buf bytes.Buffer
+
+	lead := len(s) % 3
+
+	if lead == 0 {
+		lead = 3
+	}
+
+	buf.WriteString(s[:lead])
+
+	for i := lead; i < len(s)-lead; i = i + 3 {
+		buf.WriteString("," + s[i:i+3])
+	}
+
+	return buf.String()
 }
 
 func main() {
-	fmt.Println(comma("12345"))
+	fmt.Println(comma(""))
+	fmt.Println(comma("1"))
+	fmt.Println(comma("123"))
+	fmt.Println(comma("1234567"))
 }
