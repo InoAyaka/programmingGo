c7edf3997a7310554984af85240c37a2231c1e31
Ino Ayaka <60222003+InoAyaka@users.noreply.github.com>
2020-10-22 17:02:03 +0900

    【修正】ch05 ex10 課題対応版

--- ch05/ex10/topoSort.go
+++ ch05/ex10/topoSort.go
@@ -2,53 +2,91 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
 
 func main() {
 	for i, course := range topoSort(prereqs) {
 		fmt.Printf("%d: \t%s\n", i+1, course)
 	}
+
+	fmt.Printf("\ntopological Sort : %v\n", isTopoSorted(topoSort(prereqs)))
 }
 
-var prereqs = map[string][]string{
-	"algorithms": {"data structures"},
-	"calculus":   {"linear algebra"},
-	"compilers": {
+var prereqs = map[string]itemSet{
+	"algorithms": newItemSet("data structures"),
+	"calculus":   newItemSet("linear algebra"),
+	"compilers": newItemSet(
 		"data structures",
 		"format languages",
 		"computer organization",
-	},
-	"data structures":       {"discrate math"},
-	"databases":             {"data structures"},
-	"discrate math":         {"intro to programming"},
-	"format languages":      {"discrate math"},
-	"networks":              {"operating systems"},
-	"operating systems":     {"data structures", "computer organization"},
-	"programming languages": {"data structures", "computer organization"},
+	),
+	"data structures":       newItemSet("discrate math"),
+	"databases":             newItemSet("data structures"),
+	"discrate math":         newItemSet("intro to programming"),
+	"format languages":      newItemSet("discrate math"),
+	"networks":              newItemSet("operating systems"),
+	"operating systems":     newItemSet("data structures", "computer organization"),
+	"programming languages": newItemSet("data structures", "computer organization"),
 }
 
-func topoSort(m map[string][]string) []string {
+type itemSet map[string]bool
+
+func (i itemSet) add(items ...string) {
+	for _, item := range items {
+		i[item] = true
+	}
+}
+
+func newItemSet(items ...string) itemSet {
+	is := make(itemSet)
+	is.add(items...)
+
+	return is
+}
+
+func topoSort(m map[string]itemSet) []string {
 	var order []string
 	seen := make(map[string]bool)
 
-	var visitAll func(items []string)
-	visitAll = func(items []string) {
-		for _, item := range items {
-			if !seen[item] {
-				seen[item] = true
-				visitAll(m[item])
-				order = append(order, item)
+	var visitAll func(is itemSet)
+	visitAll = func(is itemSet) {
+		for key := range is {
+			if !seen[key] {
+				seen[key] = true
+				visitAll(m[key])
+				order = append(order, key)
 			}
 		}
 	}
 
-	var key []string
-	for k := range m {
-		key = append(key, k)
+	/*
+		var key []string
+		for k := range m {
+			key = append(key, k)
+		}
+		sort.Strings(key)
+	*/
+	is := newItemSet()
+	for item := range m {
+		is.add(item)
 	}
-	sort.Strings(key)
-	visitAll(key)
+	visitAll(is)
 
 	return order
+}
+
+func isTopoSorted(order []string) bool {
+	checkList := make(map[string]int)
+	for i, item := range order {
+		checkList[item] = i
+	}
+
+	for item, i := range checkList {
+		for prereq := range prereqs[item] {
+			if i < checkList[prereq] {
+				return false
+			}
+		}
+	}
+	return true
 }
